d258da4f6b0a8fb1714f8728a83596ea8d4a565d
heitornobrega <nobheitor@gmail.com>
2023-01-23 20:29:20 -0300

    feat: implement function filter_by_industry

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -47,4 +47,9 @@ def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
     list
         List of jobs with provided industry
     """
-    raise NotImplementedError
+    # raise NotImplementedError
+    selected_industries = []
+    for job in jobs:
+        if job["industry"] == industry:
+            selected_industries.append(job)
+    return selected_industries
