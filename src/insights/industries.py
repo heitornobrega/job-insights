5b263395eb3834567437887915c278ae2a5ac5ab
heitornobrega <nobheitor@gmail.com>
2023-01-23 18:51:45 -0300

    feat: implement function get_unique_industries

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,5 +1,6 @@
 from typing import List, Dict
-
+import src.insights.jobs as jobs
+# import jobs
 
 def get_unique_industries(path: str) -> List[str]:
     """Checks all different industries and returns a list of them
@@ -16,8 +17,16 @@ def get_unique_industries(path: str) -> List[str]:
     list
         List of unique industries
     """
-    raise NotImplementedError
+    # raise NotImplementedError
+    data = jobs.read(path)
+    unique_industries = set()
+    for industry in data:
+        if len(industry['industry']) > 0:
+            unique_industries.add(industry['industry'])
 
+    return list(unique_industries)
+
+print(get_unique_industries('data/jobs.csv'))
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
     """Filters a list of jobs by industry
