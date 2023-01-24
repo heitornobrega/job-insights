a1cfec5f46c8ba2e556485649c0d0822508faf77
heitornobrega <nobheitor@gmail.com>
2023-01-24 20:15:22 -0300

    test: test_brazilian_jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,61 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
+
+from src.insights.jobs import read
 
 
 def test_brazilian_jobs():
-    pass
+    file_path = "tests/mocks/brazilians_jobs.csv"
+    expected_output = [
+        {"salary": "2000", "title": "Maquinista", "type": "trainee"},
+        {"salary": "3000", "title": "Motorista", "type": "full time"},
+        {
+            "salary": "4000",
+            "title": "Analista de Software",
+            "type": "full time",
+        },
+        {
+            "salary": "1700",
+            "title": "Assistente administrativo",
+            "type": " full time",
+        },
+        {
+            "salary": "1400",
+            "title": "Auxiliar administrativo",
+            "type": " full time",
+        },
+        {"salary": "1400", "title": "Auxiliar usinagem", "type": " full time"},
+        {
+            "salary": "1400",
+            "title": "Auxiliar de padaria",
+            "type": " full time",
+        },
+        {"salary": "1400", "title": "Analista Contábil", "type": " full time"},
+        {"salary": "5000", "title": "Gerente comercial", "type": " full time"},
+        {
+            "salary": "4000",
+            "title": "Analista de Departamento Pessoal",
+            "type": " full time",
+        },
+        {
+            "salary": "50000",
+            "title": "Esportista de Futebol profissional",
+            "type": " full time",
+        },
+        {
+            "salary": "4000",
+            "title": "Analista de crédito",
+            "type": " full time",
+        },
+        {
+            "salary": "3000",
+            "title": "Pessoa Programadora",
+            "type": " full time",
+        },
+        {"salary": "3000", "title": "Ux Designer", "type": " full time"},
+        {
+            "salary": " 1400",
+            "title": "Auxiliar de manutenção",
+            "type": " full time",
+        },
+    ]
+    assert read_brazilian_file(file_path) == expected_output
