aa9697adad88a7a38cf1229af6d9ee78c9da88f2
heitornobrega <nobheitor@gmail.com>
2023-01-24 20:38:12 -0300

    test: test_sorting

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,89 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    # Crie uma lista de empregos para testar
+    jobs = [
+        {
+            "name": "Job 1",
+            "min_salary": 1000,
+            "max_salary": 2000,
+            "date_posted": "2022-01-01",
+        },
+        {
+            "name": "Job 2",
+            "min_salary": 500,
+            "max_salary": 1500,
+            "date_posted": "2022-01-03",
+        },
+        {
+            "name": "Job 3",
+            "min_salary": 800,
+            "max_salary": 1800,
+            "date_posted": "2022-01-02",
+        },
+        {
+            "name": "Job 4",
+            "min_salary": None,
+            "max_salary": 1200,
+            "date_posted": "2022-01-04",
+        },
+    ]
+
+    sort_by(jobs, "min_salary")
+
+    assert jobs == [
+        {
+            "name": "Job 2",
+            "min_salary": 500,
+            "max_salary": 1500,
+            "date_posted": "2022-01-03",
+        },
+        {
+            "name": "Job 3",
+            "min_salary": 800,
+            "max_salary": 1800,
+            "date_posted": "2022-01-02",
+        },
+        {
+            "name": "Job 1",
+            "min_salary": 1000,
+            "max_salary": 2000,
+            "date_posted": "2022-01-01",
+        },
+        {
+            "name": "Job 4",
+            "min_salary": None,
+            "max_salary": 1200,
+            "date_posted": "2022-01-04",
+        },
+    ]
+
+    sort_by(jobs, "max_salary")
+
+    assert jobs == [
+        {
+            "name": "Job 1",
+            "min_salary": 1000,
+            "max_salary": 2000,
+            "date_posted": "2022-01-01",
+        },
+        {
+            "name": "Job 3",
+            "min_salary": 800,
+            "max_salary": 1800,
+            "date_posted": "2022-01-02",
+        },
+        {
+            "name": "Job 2",
+            "min_salary": 500,
+            "max_salary": 1500,
+            "date_posted": "2022-01-03",
+        },
+        {
+            "name": "Job 4",
+            "min_salary": None,
+            "max_salary": 1200,
+            "date_posted": "2022-01-04",
+        },
+    ]
